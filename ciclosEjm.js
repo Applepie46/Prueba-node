aed9613162acfcd65b2746bcd85af1242c566a6e
applepie46 <emmanuel.cadavid46@gmail.com>
2022-04-27 23:55:57 -0500

    primeros pasos

---
+++ ciclosEjm.js
@@ -0,0 +1,3 @@
+let numero = 7;
+
+console.log("hola " + numero );
---
+++ prueba.js
@@ -0,0 +1,11 @@
+let miNombre = "Emmanuel";
+
+let saludar = (numero) => {
+    for (let i = 0; i <= numero; i++) {
+        console.log("Hola " + miNombre);
+    }
+}
+
+saludar(7);
+
+let numerosTel = [234555, 455556];
